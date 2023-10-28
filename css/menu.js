bd95e8508b08530690d3a7cd892577c4cc31401b
david <daviddavid21214@gmail.com>
2023-10-27 23:52:34 -0600

    nuevo

---
+++ css/menu.js
@@ -0,0 +1,20 @@
+// JavaScript para crear un carrusel de imágenes automático
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    let slides = document.querySelectorAll(".carousel-slide");
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {
+        slideIndex = 1;
+    }
+    slides[slideIndex - 1].style.display = "block";
+    setTimeout(showSlides, 3000); // Cambia la imagen cada 3 segundos (ajusta el tiempo según tus preferencias)
+}
+document.querySelector('.menu-icon').addEventListener('click', function () {
+    const menu = document.querySelector('.menu');
+    menu.classList.toggle('active');
+});
